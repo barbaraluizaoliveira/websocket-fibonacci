9c034874d3068faf7f1f783c0a85349a30416e8a
Bárbara Luiza Oliveira <barbaraluizaoliveiradev@gmail.com>
2025-03-24 19:32:41 +0000

    feature: websockets fibonacci server

--- app/service.py
+++ app/service.py
@@ -2,9 +2,6 @@ from sqlalchemy.orm import Session
 from app.models.connected_user import ConnectedUser
 
 def save_user_connected(db: Session, username: str, connected_at):
-    """
-    Salva um usuário conectado no banco de dados.
-    """
     new_user = ConnectedUser(username=username, connected_at=connected_at)
     db.add(new_user)
     db.commit()
@@ -12,8 +9,5 @@ def save_user_connected(db: Session, username: str, connected_at):
     return new_user
 
 def remove_user_connected(db: Session, username: str):
-    """
-    Remove um usuário conectado ao desconectar.
-    """
     db.query(ConnectedUser).filter(ConnectedUser.username == username).delete()
     db.commit()
